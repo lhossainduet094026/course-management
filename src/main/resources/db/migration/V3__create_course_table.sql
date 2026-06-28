6c2a4130c27ac3ae9c855589a28fa67980be6fc7
lhossainduet094026 <lhossainduet094026@gmail.com>
2026-06-28 18:32:59 +0600

    fix(docker): resolve Docker image build issue

--- src/main/resources/db/migration/V3__create_course_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `courses` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `price` double DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
